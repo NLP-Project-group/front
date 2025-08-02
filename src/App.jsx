2f5c7fc1a6fdef68eb6772c89d21d0b823bfc069
Loïc RAKOTOMANDIMBY <rakotomandimby.loic@gmail.com>
2025-08-02 12:20:26 +0300

    init_project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        First page
+      </p>
+    </>
+  )
+}
+
+export default App
